3a65d7caa63febb7a259315300f2ffb50c7b7b84
kalalaukid <jdickman.sfg@gmail.com>
2025-04-17 15:07:54 -0700

    Create thank-you-insurance.js

---
+++ pages/thank-you-insurance.js
@@ -0,0 +1,14 @@
+export default function ThankYouInsurance() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'Arial', textAlign: 'center' }}>
+      <h1>✅ You're All Set!</h1>
+      <p>Thanks for reaching out. I’ll be in touch shortly to help you plan your retirement strategy.</p>
+      <p style={{ marginTop: '2rem' }}>
+        Want to skip the line? Book a time here:<br />
+        <a href="https://cal.com/joshua-dickman/15min" target="_blank" rel="noreferrer">
+          https://cal.com/joshua-dickman/15min
+        </a>
+      </p>
+    </main>
+  );
+}
